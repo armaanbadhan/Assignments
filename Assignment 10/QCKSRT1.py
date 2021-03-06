fafd8053762161460d4c0f5eb24f4cc3a0df2488
armaanbadhan <66007619+armaanbadhan@users.noreply.github.com>
2021-03-06 16:55:30 +0530

    Add files via upload

---
+++ Assignment 10/QCKSRT1.py
@@ -0,0 +1,3 @@
+n = int(input())
+l = [1]*n
+print(*l)
---
+++ Assignment 10/QCKSRT2.py
@@ -0,0 +1,32 @@
+# you can use this less than comparator operator in the choose_pivot, if you would like
+# keep in mind that this is the comparator used in the quick sort implementation given
+def lessthan(x, y):
+    return x[0] < y[0]
+ 
+# complete this function
+# the return value is wrong, you are to complete this function so that the return value is correct
+# arr is a list of tuples where each tuple contains the pair (x, y)
+
+
+def choose_pivot(arr):
+    key = set()
+    val = []
+    for i in range(len(arr)):
+        if arr[i][0] in key:
+            pass
+        else:
+            key.add(arr[i][0])
+            val.append(i)
+    return val
+ 
+# ---------------------------- Do not modify code below this line ------------------------------------
+n = int(input())
+arr_strip = list(map(int, input().split()))
+arr = []
+for i in range(0, 2*n, 2):
+    arr.append((arr_strip[i], arr_strip[i+1]))
+ 
+candidates = choose_pivot(arr)
+
+print(len(candidates))
+print(*candidates)
